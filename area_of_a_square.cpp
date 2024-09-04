99040dd84213f9866eb03fcadf493424055a0f12
VanshNamdev05 <159474384+VanshNamdev05@users.noreply.github.com>
2024-09-04 21:41:57 +0530

    Add files via upload

---
+++ area_of_a_square.cpp
@@ -0,0 +1,10 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+        int a;
+        cout<<"Enter the side of the square : "<<endl;
+        cin>>a;
+        cout<<"The area of the square is : "<<(a*a);
+    return 0;
+}
