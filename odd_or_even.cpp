b9daa6f0a6f3fc3e640963e9b02564fb78930b2a
VanshNamdev05 <159474384+VanshNamdev05@users.noreply.github.com>
2024-09-04 21:42:36 +0530

    Add files via upload

---
+++ odd_or_even.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    int a;
+    cout<<"Enter the value of a :"<<endl;
+    cin>>a;
+    if (a%2==0 )
+    {
+        cout<<a<<" is a even number";
+    }
+    else{
+        cout<<a<<" is an odd number";
+    }
+    
+
+    return 0;
+}
