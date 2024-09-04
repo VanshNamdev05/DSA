c5e40736685e466fdc3f53ee6a11051d10c54b42
VanshNamdev05 <159474384+VanshNamdev05@users.noreply.github.com>
2024-09-04 21:42:24 +0530

    Add files via upload

---
+++ min_of_two_number.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    int a,b;
+    cout<<"a : ";
+    cin>>a;
+    cout<<"b : ";
+    cin>>b;
+    if (a<b)
+    {
+        cout<<"The small number is "<<a;
+    }
+    else{
+        cout<<"The small number is "<<b;
+    }
+    
+    return 0;
+}
